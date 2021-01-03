7007fd534e0bc474fd5f6e32c1f3e9937952e788
Ken Moini <ken@kenmoini.com>
2021-01-02 21:03:15 -0500

    fix powershell cache keeping logic switch

--- setup_fedora_wsl_distro.ps1
+++ setup_fedora_wsl_distro.ps1
@@ -68,7 +68,7 @@ if (!(Test-Path "C:\Temp\fedoraWSL\xz\xz.zip")) {
 }
 
 echo "Downloading Fedora 33 system root..."
-if (!(Test-Path "C:\Temp\fedoraWSL\fedora_root.tar.xz") && !(Test-Path "C:\Temp\fedoraWSL\fedora_root.tar")) {
+if (!(Test-Path "C:\Temp\fedoraWSL\fedora_root.tar.xz") -and !(Test-Path "C:\Temp\fedoraWSL\fedora_root.tar")) {
   # Get Fedora system root file
   Invoke-WebRequest -Uri "https://github.com/fedora-cloud/docker-brew-fedora/raw/$fedora_major_version/x86_64/fedora-$fedora_version-x86_64.tar.xz" -OutFile "C:\Temp\fedoraWSL\fedora_root.tar.xz"
 
